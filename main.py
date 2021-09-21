0f9a2ddfcf69f41b88d1a268779a1f012630a9df
Emilypipp22 <814803@mystma.org>
2021-09-21 09:11:59 -0500

    second try

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
 def topFiveMovies(a, b, c, d, e):
-    return (a, b, c, d, e)
-
-print (topFiveMovies("Spider man", "Iron man", "Doctor Strange", "The Hulk", "Captain AMerica"))
+    return (a +" " + b + " " + c + " " + d + " " + e)
